087a8ccc6079b320e4e177ec4386cb0efbb8e4d5
GiorgioAscione <acergio95@libero.it>
2022-04-28 22:10:39 +0200

    Aggiungere i file di progetto.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,92 @@
+﻿namespace GestoreEventi
+{
+    internal class Evento
+    {
+        private string titolo;
+        public int postiPrenotati;
+        private DateTime data;
+        public int postiTotali;
+
+
+        public Evento(string titolo, int postiTotali, DateTime data)
+        {
+            SetTitolo(titolo);
+            SetData(data);
+            this.data = data;
+            this.postiPrenotati = 0;
+            if (postiPrenotati > 1)
+            {
+                throw new Exception("Il numero di posti totali inserito non è corretto");
+            }
+            this.postiTotali = postiTotali;
+        }
+
+        public string GetTitolo()
+        {
+            return titolo;
+        }
+
+        public void SetTitolo(string titolo)
+        {
+            if (titolo == "" || titolo.Length == null)
+            {
+                throw new ArgumentException("Il titolo inserito non è corretto");
+            }
+            this.titolo = titolo;
+        }
+
+        public DateTime GetData()
+        {
+            return data;
+        }
+
+        public void SetData(DateTime data)
+        {
+            if (data < DateTime.Now)
+            {
+                throw new ArgumentException("La data inserita è antecedente alla data consentita");
+            }
+            this.data = data;
+        }
+
+        public int PostiDisponibili()
+        {
+            return postiTotali - postiPrenotati;
+        }
+
+        public int PostiPrenotati()
+        {
+            return postiPrenotati;
+        }
+
+        public void PrenotaPosto()
+        {
+            if (postiPrenotati >= postiTotali)
+            {
+                throw new Exception("I posti disponibili sono esauriti");
+            }
+
+            if (data < DateTime.Now)
+            {
+                throw new Exception("L'evento è già passato");
+            }
+            postiPrenotati++;
+        }
+
+        public void PrenotaPosti(int posti)
+        {
+            if (postiPrenotati + posti > postiTotali)
+            {
+                throw new Exception("Non posso prenotare tutti questi posti");
+            }
+            if (data < DateTime.Now)
+            {
+                throw new Exception("L'evento è già passato");
+            }
+
+            postiPrenotati += posti;
+        }
+
+
+    }
+}
---
+++ GestoreEventi/Program.cs
@@ -0,0 +1,33 @@
+﻿using GestoreEventi;
+
+Console.WriteLine("----GESTIONE EVENTI-----");
+
+Evento primoEvento = null;
+while (primoEvento == null)
+{
+    Console.WriteLine("Inserisci il nome dell' evento: ");
+    string titolo = Console.ReadLine();
+    Console.WriteLine("Inserisci la data dell' evento (gg/mm/aaaa): ");
+    string dataString = Console.ReadLine();
+    DateTime data = DateTime.Parse(dataString);
+    Console.WriteLine("Inserisci il numero di posti totali: ");
+    int posti = int.Parse(Console.ReadLine());
+    primoEvento = new Evento(titolo, posti, data);
+}
+
+Console.WriteLine("Quanti posti vuoi prenotare?");
+int numeroPrenotazioni = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+primoEvento.PrenotaPosti(numeroPrenotazioni);
+
+Console.WriteLine("Numero di posti prenotati = " + primoEvento.PostiPrenotati());
+Console.WriteLine("Numero di posti disponibili = " + primoEvento.PostiPrenotati());
+
+
+
+
+
+
+
